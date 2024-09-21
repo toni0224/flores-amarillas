5019e524709f6e7717071b8e4466236b4a3f1128
toni <u18100033@utp.edu.pe>
2024-09-21 00:50:59 -0500

    Floress

--- Js/app.js
+++ Js/app.js
@@ -2,7 +2,7 @@ let Titulo = document.title;
 
 window.addEventListener('blur', () => {
     Titulo = document.title;
-    document.title = "No te vallas, regresa :(";
+    document.title = "Tu eres importante";
 })
 
 window.addEventListener('focus', () => {
